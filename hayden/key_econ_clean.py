849dfd1564463e7c98a9410bc5389f3be12d7654
Hayden Warren <haydenlw4@gmail.com>
2021-09-14 10:19:29 -0400

    adding cleaning for tax permits, key econ indicators, and booze taxes

---
+++ hayden/key_econ_clean.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 13 19:13:27 2021
+
+@author: haydenlw4
+"""
+
+import pandas as pd
+
+econ = pd.read_csv('Key_Economic_Indicators.csv')
+
+econ = econ[econ['Year']>=2012].reset_index(drop=True)
+
+econ = econ.dropna(axis = 'columns', thresh=100)
+
+econ.to_csv('Key_Economic_Indicators_Cleaned.csv')
